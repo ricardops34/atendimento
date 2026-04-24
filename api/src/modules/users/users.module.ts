3bbeb1b10b1fb7a363afd6c6157edd8eb1877ef9
ricardops34 <ricardopataysotomayor@gmail.com>
2026-04-24 15:08:42 -0400

    fix: exported UsersService from UsersModule to resolve AuthModule dependency

--- api/src/modules/users/users.module.ts
+++ api/src/modules/users/users.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { PrismaModule } from '../../prisma/prisma.module';
 
 @Module({
-  providers: [UsersService]
+  imports: [PrismaModule],
+  providers: [UsersService],
+  exports: [UsersService] // Agora outros módulos podem usar o UsersService!
 })
 export class UsersModule {}
