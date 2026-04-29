21d5fe2e1f266fdf1a28c0ddfa625c2d959116a1
ricardo.e2 <ricardo.e2@msgas.com.br>
2026-04-29 14:12:15 -0400

    style: simplify po-menu attributes syntax

--- web/src/app/pages/main/main.ts
+++ web/src/app/pages/main/main.ts
@@ -27,9 +27,9 @@ import { CoreService } from '../../core/services/core.service';
       <po-menu 
         id="saas-main-menu" 
         [p-menus]="menus"
-        [p-filter]="true"
+        p-filter
         [p-collapsed]="isCollapsed"
-        [p-automatic-toggle]="true">
+        p-automatic-toggle>
         
         <div p-menu-header class="po-p-3 po-text-center">
           <div class="po-font-subtitle po-text-color-01">Bem-vindo,</div>
